2ccd91da2aef0964b692ad02e720b71220e93318
Adrian Gomez <adrian_alliskair@hotmail.com>
2023-12-21 14:47:55 -0400

    first commit

---
+++ src/posts/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Post, PostView, Like, Comment, User
+
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(PostView)
+admin.site.register(Like)
+admin.site.register(Comment)
